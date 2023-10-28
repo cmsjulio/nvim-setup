82a95dd73611314c0f43c3ecffc31a597acf4ff4
cmsjulio <cms.julio1@gmail.com>
2023-10-28 00:48:11 -0300

    feat: add A-j and A-k as nmaps for adding line above and below

--- lua/zh3ro/init.lua
+++ lua/zh3ro/init.lua
@@ -1,3 +1,8 @@
 require("zh3ro.set")
+print('loaded')
 local vimrc = vim.fn.stdpath("config") .. "/lua/zh3ro/vimrc.vim"
+
+vim.cmd('nnoremap <silent> <A-j> :<C-u>call append(line("."),   repeat([""], v:count1))<CR>')
+vim.cmd('nnoremap <silent> <A-k> :<C-u>call append(line(".")-1, repeat([""], v:count1))<CR>')
+
 vim.cmd.source(vimrc)
