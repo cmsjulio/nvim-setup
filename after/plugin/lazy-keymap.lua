0caa8e664a2dc8f50742eaae350c9538e8574a74
cmsjulio <cms.julio1@gmail.com>
2023-10-29 17:54:46 -0300

    feat: add (lazily) plugins to windows navigation (jumping between windows)

---
+++ after/plugin/lazy-keymap.lua
@@ -0,0 +1,6 @@
+-- navigation around windows
+vim.cmd('nnoremap <silent> <leader>h <c-w>h')
+vim.cmd('nnoremap <silent> <leader>j <c-w>j')
+vim.cmd('nnoremap <silent> <leader>k <c-w>k')
+vim.cmd('nnoremap <silent> <leader>l <c-w>l')
+vim.cmd('nnoremap <silent> <leader>o <c-w>o')
