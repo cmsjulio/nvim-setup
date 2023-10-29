8acc2fa454939803e0ea706494e339e40a20c652
cmsjulio <cms.julio1@gmail.com>
2023-10-29 17:52:29 -0300

    feat: add keymaps for tab navigation and select all with <c-a>

--- lua/zh3ro/init.lua
+++ lua/zh3ro/init.lua
@@ -1,8 +1,15 @@
+-- check after/plugin/lazy-keymap.lua for other mappings
 require("zh3ro.set")
-require("zh3ro.custom-bindings")
 local vimrc = vim.fn.stdpath("config") .. "/lua/zh3ro/vimrc.vim"
+vim.cmd.source(vimrc)
 
 vim.cmd('nnoremap <silent> <A-j> :<C-u>call append(line("."),   repeat([""], v:count1))<CR>')
 vim.cmd('nnoremap <silent> <A-k> :<C-u>call append(line(".")-1, repeat([""], v:count1))<CR>')
 
-vim.cmd.source(vimrc)
+vim.cmd('nnoremap <silent> <c-t> :lua vim.cmd("tabnew")<CR>')
+vim.cmd('nnoremap <silent> <c-w> :bdelete!<CR>')
+vim.cmd('nnoremap <silent> " :tabnext<CR>')
+vim.cmd('nnoremap <silent> <s-tab> :tabprevious<CR>')
+
+-- select all with <c-a>
+vim.cmd('nnoremap <silent> <c-a> ggVG')
