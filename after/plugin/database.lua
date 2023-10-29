f0cc2df6f4c0af526c668b666e6febb9134a1e1d
cmsjulio <cms.julio1@gmail.com>
2023-10-29 17:54:01 -0300

    feat: add shortcut for opening db plugin <leader>db

---
+++ after/plugin/database.lua
@@ -0,0 +1,11 @@
+local key_map = function(mode, key, result)
+  vim.api.nvim_set_keymap(
+    mode,
+    key,
+    result,
+    {noremap = true, silent = true}
+  )
+end
+
+key_map('n', '<leader>db', ':DBUI<CR>')
+
