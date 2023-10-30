65ae1c86558b87c99518e0577b7e84493fe7d70f
cmsjulio <cms.julio1@gmail.com>
2023-10-29 21:22:50 -0300

    feat: close window with <leader>c

--- after/plugin/lazy-keymap.lua
+++ after/plugin/lazy-keymap.lua
@@ -4,4 +4,5 @@ vim.cmd('nnoremap <silent> <leader>j <c-w>j')
 vim.cmd('nnoremap <silent> <leader>k <c-w>k')
 vim.cmd('nnoremap <silent> <leader>l <c-w>l')
 vim.cmd('nnoremap <silent> <leader>o <c-w>o')
+vim.cmd('nnoremap <silent> <leader>c <c-w>c')
 vim.cmd('nnoremap <silent> <leader>w <c-6>') --reopen last visited buffer
