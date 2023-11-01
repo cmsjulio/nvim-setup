c46897ff6896c2ef097833b3dcd84a4a7d1f2814
cmsjulio <cms.julio1@gmail.com>
2023-11-01 12:29:17 -0300

    refactor/feat(%) change directory references to make it compatible across multiple devices

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -14,7 +14,9 @@ local jdtls_path = vim.fn.stdpath('data') .. "/mason/packages/jdtls"
 local path_to_lsp_server = jdtls_path .. "/config_linux"
 local path_to_plugins = jdtls_path .. "/plugins/"
 local path_to_jar = path_to_plugins .. "org.eclipse.equinox.launcher_1.6.500.v20230717-2134.jar"
-local lombok_path = path_to_plugins .. "lombok.jar"
+local lombok_path = path_to_plugins .. "../lombok.jar"
+local home = vim.loop.os_homedir();
+print (home)
 
 local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
 local root_dir = require("jdtls.setup").find_root(root_markers)
@@ -25,12 +27,15 @@ end
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
 local workspace_dir = vim.fn.stdpath('data') .. '/site/java/workspace-root/' .. project_name
 
+function pws()
+print(workspace_dir)
+end
 -- Main Config
 local config = {
   -- The command that starts the language server
   -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
   cmd = {
-    '/home/solidzh3ro/.sdkman/candidates/java/19.0.1-open/bin/java',
+    home .. '/.sdkman/candidates/java/19.0.1-open/bin/java',
     '-Declipse.application=org.eclipse.jdt.ls.core.id1',
     '-Dosgi.bundles.defaultStartLevel=4',
     '-Declipse.product=org.eclipse.jdt.ls.core.product',
@@ -56,7 +61,7 @@ local config = {
   -- for a list of options
   settings = {
     java = {
-      home = '/home/solidzh3ro/.sdkman/candidates/java/19.0.1-open',
+      home = home ..  '/.sdkman/candidates/java/19.0.1-open',
       eclipse = {
         downloadSources = true,
       },
@@ -65,15 +70,15 @@ local config = {
         runtimes = {
           {
             name = "JavaSE-1.8",
-            path = "/home/solidzh3ro/.sdkman/candidates/java/8.0.352-amzn",
+            path = home .. "/.sdkman/candidates/java/8.0.352-amzn",
           },
           {
             name = "JavaSE-17",
-            path = "/home/solidzh3ro/.sdkman/candidates/java/17.0.7-amzn",
+            path = home .. "/.sdkman/candidates/java/17.0.7-amzn",
           },
           {
             name = "JavaSE-19",
-            path = "/home/solidzh3ro/.sdkman/candidates/java/19.0.1-open",
+            path = home .. "/.sdkman/candidates/java/19.0.1-open",
           }
         }
       },
@@ -128,7 +133,7 @@ local config = {
     allow_incremental_sync = true,
   },
   init_options = {
-    bundles = {vim.fn.glob("/home/solidzh3ro/Downloads/java-debug-0.49.0/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-0.49.0.jar", 1)},
+    bundles = {vim.fn.glob(home .. "/Downloads/java-debug-0.49.0/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-0.49.0.jar", 1)},
   },
 }
 
