50effee2033a1c7d006f9497c2ee4752d2021398
cmsjulio <cms.julio1@gmail.com>
2023-11-01 12:32:39 -0300

    refactor: lazy-lock update and old file delete

--- after/plugin/lazy-keymap.lua
+++
@@ -1,8 +0,0 @@
--- navigation around windows
-vim.cmd('nnoremap <silent> <leader>h <c-w>h')
-vim.cmd('nnoremap <silent> <leader>j <c-w>j')
-vim.cmd('nnoremap <silent> <leader>k <c-w>k')
-vim.cmd('nnoremap <silent> <leader>l <c-w>l')
-vim.cmd('nnoremap <silent> <leader>o <c-w>o')
-vim.cmd('nnoremap <silent> <leader>c <c-w>c')
-vim.cmd('nnoremap <silent> <leader>w <c-6>') --reopen last visited buffer
