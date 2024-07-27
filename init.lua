8bda3455aaead1b3e5437d60d83f0c53b201dd15
cmsjulio <cms.julio1@gmail.com>
2024-07-27 17:08:01 -0300

    formata

--- init.lua
+++ init.lua
@@ -4,109 +4,114 @@ vim.keymap.set("n", "<leader>v", vim.cmd.Ex)
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-	{'nvim-telescope/telescope.nvim', tag = '0.1.0',
-	-- or                            , branch = '0.1.x',
-	dependencies = { {'nvim-lua/plenary.nvim'} } },
-	{'rose-pine/neovim', name = 'rose-pine'},
-	{	"nvim-treesitter/nvim-treesitter",
-	version = false, -- last release is way too old and doesn't work on Windows
-	build = ":TSUpdate",
-	event = { "BufReadPost", "BufNewFile" },
-	dependencies = {
-		{
-			"nvim-treesitter/nvim-treesitter-textobjects",
-			init = function()
-				-- disable rtp plugin, as we only need its queries for mini.ai
-				-- In case other textobject modules are enabled, we will load them
-				-- once nvim-treesitter is loaded
-				require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
-				load_textobjects = true
-			end,
-		},
-	},
-	cmd = { "TSUpdateSync" }
-},
-'theprimeagen/harpoon',
-'mbbill/undotree',
-'tpope/vim-fugitive',
-'mfussenegger/nvim-jdtls',
-{'williamboman/mason.nvim'},
-{'williamboman/mason-lspconfig.nvim'},
-
--- lualine for cooler status bar
-{
-  'nvim-lualine/lualine.nvim',
-  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-},
-{
-  "nomnivore/ollama.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-
-  -- All the user commands added by the plugin
-  cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
-
-  -- Sample keybind for prompting. Note that the <c-u> is important for selections to work properly.
-  keys = {
     {
-      "<leader>ia",
-      ":<c-u>lua require('ollama').prompt()<cr>",
-      desc = "ollama prompt",
-      mode = { "n", "v" },
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.0',
+        -- or                            , branch = '0.1.x',
+        dependencies = { { 'nvim-lua/plenary.nvim' } }
     },
-  },
+    { 'rose-pine/neovim',                 name = 'rose-pine' },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        version = false, -- last release is way too old and doesn't work on Windows
+        build = ":TSUpdate",
+        event = { "BufReadPost", "BufNewFile" },
+        dependencies = {
+            {
+                "nvim-treesitter/nvim-treesitter-textobjects",
+                init = function()
+                    -- disable rtp plugin, as we only need its queries for mini.ai
+                    -- In case other textobject modules are enabled, we will load them
+                    -- once nvim-treesitter is loaded
+                    require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+                    load_textobjects = true
+                end,
+            },
+        },
+        cmd = { "TSUpdateSync" }
+    },
+    'theprimeagen/harpoon',
+    'mbbill/undotree',
+    'tpope/vim-fugitive',
+    'mfussenegger/nvim-jdtls',
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
 
-  ---@type Ollama.Config
-  opts = {
-    model = "codellama:instruct"
-    -- your configuration overrides
-  }
-},
--- DB integration
-{
-  'kristijanhusak/vim-dadbod-ui',
-  dependencies = {
-    { 'tpope/vim-dadbod', lazy = true },
-    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
-  },
-  cmd = {
-    'DBUI',
-    'DBUIToggle',
-    'DBUIAddConnection',
-    'DBUIFindBuffer',
-  },
-  init = function()
-    -- Your DBUI configuration
-    vim.g.db_ui_use_nerd_fonts = 1
-  end,
-},
+    -- lualine for cooler status bar
+    {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    },
+    {
+        "nomnivore/ollama.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
 
-  -- Debugging
-'mfussenegger/nvim-dap',
-'rcarriga/cmp-dap',
-{ "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap"} },
-{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-{'neovim/nvim-lspconfig'},
-{'hrsh7th/cmp-nvim-lsp'},
-{'hrsh7th/nvim-cmp'},
-{'L3MON4D3/LuaSnip'},
- "lewis6991/gitsigns.nvim",
-{    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" , "nvim-tree/nvim-web-devicons"}
-}
+        -- All the user commands added by the plugin
+        cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
+
+        -- Sample keybind for prompting. Note that the <c-u> is important for selections to work properly.
+        keys = {
+            {
+                "<leader>ia",
+                ":<c-u>lua require('ollama').prompt()<cr>",
+                desc = "ollama prompt",
+                mode = { "n", "v" },
+            },
+        },
+
+        ---@type Ollama.Config
+        opts = {
+            model = "codellama:instruct"
+            -- your configuration overrides
+        }
+    },
+    -- DB integration
+    {
+        'kristijanhusak/vim-dadbod-ui',
+        dependencies = {
+            { 'tpope/vim-dadbod',                     lazy = true },
+            { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+        },
+        cmd = {
+            'DBUI',
+            'DBUIToggle',
+            'DBUIAddConnection',
+            'DBUIFindBuffer',
+        },
+        init = function()
+            -- Your DBUI configuration
+            vim.g.db_ui_use_nerd_fonts = 1
+        end,
+    },
+
+    -- Debugging
+    'mfussenegger/nvim-dap',
+    'rcarriga/cmp-dap',
+    { "rcarriga/nvim-dap-ui",      dependencies = { "mfussenegger/nvim-dap" } },
+    { 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' },
+    { 'neovim/nvim-lspconfig' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/nvim-cmp' },
+    { 'L3MON4D3/LuaSnip' },
+    "lewis6991/gitsigns.nvim",
+    {
+        "nvim-telescope/telescope-file-browser.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" }
+    }
 
 
 }
